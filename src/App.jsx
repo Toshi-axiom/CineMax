8830d0d38a700e831300abdd640591cabb96ea43
Toshi-axiom <44toshigarg@gmail.com>
2026-05-04 22:32:21 +0530

    fix: navbar fixed positioning by moving outside transition block

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,9 @@ function App() {
     <MovieProvider>
       {showPreloader && <Preloader onFinish={() => setShowPreloader(false)} />}
       
+      {/* Navbar outside the transition container so `fixed` positioning works correctly against the viewport */}
+      <Navbar />
+      
       <div 
         style={{ 
           height: showPreloader ? '100vh' : 'auto', 
@@ -23,10 +26,8 @@ function App() {
           opacity: showPreloader ? 0 : 1,
           transition: 'opacity 0.5s ease-in'
         }}
-        className="min-h-screen bg-black text-white relative"
+        className="min-h-screen bg-[#050505] text-white relative"
       >
-        <Navbar />
-        
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/movie/:id" element={<MoviePage />} />
